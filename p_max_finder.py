f492cbd088845df7349bf30b27136040c09ded18
Henry Tran <hienrytran2003@gmail.com>
2025-09-23 14:30:46 -0500

    Added an electrode location column to the main dataframe and implemented sorting of the electrode locations from A1-O15. Still doesn't work on Sample 8B for 5hz however, not sure why

--- p_max_finder.py
+++ p_max_finder.py
@@ -12,12 +12,14 @@ top_skip = 42 #the number of rows to skip from the top to when the data starts
 bottom_skip = 18 #the number of rows to skip from the bottom to when the data endss
 start_threshold = -999 #if the starting point is greater than this number, it will also find the max
 columns = dict(Polarization = "Measured Polarization", Voltage = "Drive Voltage")
-cwd = "PE loop Sample 5/Sample A"
+cwd = "PE loop Sample 8/Sample B_monopolar"
 glob_stuff = "*5Hz.txt"
-excel_name = "Unnormalized_Pmax_Sample_5A_5Hz.xlsx"
+excel_name = "Unnormalized_Pmax_Sample_8B_5Hz.xlsx"
 
 # txt_files[count] shows the file that is producing the error
 #------------------------------------------------------------------
+sort = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10","A11","A12","A13","A14","A15","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","B12","B13","B14","B15","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13","C14","C15","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13","D14","D15","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","E11","E12","E13","E14","E15","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","F13","F14","F15","G1","G2","G3","G4","G5","G6","G7","G8","G9","G10","G11","G12","G13","G14","G15","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12","H13","H14","H15","I1","I2","I3","I4","I5","I6","I7","I8","I9","I10","I11","I12","I13","I14","I15","J1","J2","J3","J4","J5","J6","J7","J8","J9","J10","J11","J12","J13","J14","J15","K1","K2","K3","K4","K5","K6","K7","K8","K9","K10","K11","K12","K13","K14","K15","L1","L2","L3","L4","L5","L6","L7","L8","L9","L10","L11","L12","L13","L14","L15","M1","M2","M3","M4","M5","M6","M7","M8","M9","M10","M11","M12","M13","M14","M15","N1","N2","N3","N4","N5","N6","N7","N8","N9","N10","N11","N12","N13","N14","N15","O1","O2","O3","O4","O5","O6","O7","O8","O9","O10","O11","O12","O13","O14","O15"]
+
 
 
 #Creates default electrode data and saves to csv (use until electrode data measurements are done)
@@ -218,10 +220,17 @@ max_sp_diff = []
 max_sp_uncert = []
 Ec_average_normalized=[]
 unnorm_p_max = []
+electrode_locs = []
 
 for i in range(len(data_list)):
     if file not in mult_file_list:
             filename = filenames[i]
+            if filename[2] == "_":
+                electrode_loc = filename[0:2]
+            else:
+                electrode_loc = filename[0:3]
+            electrode_locs.append(electrode_loc)
+
             polarization = np.array(data_list[i]["Measured Polarization (uC/cm2)"])
             unnorm_p_max.append(np.nanmax(polarization))
             voltage = np.array(data_list[i]["Drive Voltage"])
@@ -259,6 +268,8 @@ for i in range(len(data_list)):
                 max_sp_diff.append('na')
                 max_sp_uncert.append('na')
 
+            
+
 
 
 
@@ -276,17 +287,22 @@ data = {"File Name" : filenames,
         "Hc Minus": Ec_minus,
         "Hc Average": Ec_average,
         "Hc Average Normalized": Ec_average_normalized,
-        "Unnormalized P_max": unnorm_p_max
+        "Unnormalized P_max": unnorm_p_max,
+        "Electrode Locations": electrode_locs
 
 }
 
-
-
 df = pd.DataFrame(data)
 
-print(df)
+sorted = [item for item in sort if item in electrode_locs]
 
-df2 = df[['File Name', "Unnormalized P_max"]]
+
+df["Electrode Locations"] = pd.Categorical(df["Electrode Locations"], categories=sorted, ordered= True)
+df = df.sort_values("Electrode Locations")
+
+df2 = df[['File Name', "Unnormalized P_max", "Electrode Locations"]]
+
+print(df2)
 df2.to_excel(excel_name, index = False)
 
 
