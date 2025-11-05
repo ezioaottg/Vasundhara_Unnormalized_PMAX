73de066a287db23531f37917347a194ec5995937
Hien Tran <hienrytran2003@gmail.com>
2025-11-05 12:19:38 -0600

    Added electrode measurements, not complete yet

---
+++ electrode measurements.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+import pandas as pd
+import utility
+from glob import glob
+
+list_order = utility.cell_labels(['A', 'O'], [1,16])
+ 
+
+os.chdir("Pmax/Old Pmax")
+os.chdir("Sample 2")
+text_files = glob("U*Sample_2B*.xlsx")
+
+
+loc_list = []
+for file in text_files:
+    df =pd.read_excel(file)
+    loc_list.append(list(df["Electrode Locations"]))
+
+loc_intersection = set(loc_list[0]) & set(loc_list[1]) & set(loc_list[2])
+intersection_list = list(loc_intersection)
+ordered_intersection_list = utility.order_list_by_reference(intersection_list, list_order)
+print(ordered_intersection_list)
